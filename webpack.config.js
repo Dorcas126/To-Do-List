d90079452467d0f2ef8fe702d78788b0a96e2d18
Dorcas126 <uwimbabazidorcas65@gmail.com>
2023-06-14 15:34:03 +0200

    Add JSON file and webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,29 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    index: './src/index.js',
-    print: './src/print.js',
+  entry: './src/index.js',
+
+  devServer: {
+    static: './dist',
   },
-  devtool: 'inline-source-map',
- devServer: {
-   static: './dist',
- },
+
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Development',
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
- optimization: {
-   runtimeChunk: 'single',
- },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
